0a3901a24064ace8383485f5c852abcc013d8db9
Murat Kuyluk <murat.kuyluk@gmail.com>
2019-05-20 23:32:23 +0100

    Task-02: Add a new account - GREEN
    Fixes the problem about id generation.

--- src/main/java/com/assessment/account/entity/AccountEntity.java
+++ src/main/java/com/assessment/account/entity/AccountEntity.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 public class AccountEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
 
