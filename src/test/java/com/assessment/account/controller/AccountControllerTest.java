ee7650c187b8d200c7fe435b07f3e8aee3199ef7
Murat Kuyluk <murat.kuyluk@gmail.com>
2019-05-20 13:13:34 +0100

    Task-01: Retrieve all accounts - RED
    Adds assertion for content-type is JSON.

--- src/test/java/com/assessment/account/controller/AccountControllerTest.java
+++ src/test/java/com/assessment/account/controller/AccountControllerTest.java
@@ -5,11 +5,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AccountControllerTest {
@@ -28,6 +29,7 @@ public class AccountControllerTest {
     public void shouldRetrieveAllAccounts() throws Exception {
 
         mockMvc.perform(MockMvcRequestBuilders.get("/accounts"))
-                .andExpect(status().is2xxSuccessful());
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
     }
 }
