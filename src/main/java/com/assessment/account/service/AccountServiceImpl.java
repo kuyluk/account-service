2bf648f6ca46a4f241f74121568242d57469ee9a
Murat Kuyluk <murat.kuyluk@gmail.com>
2019-05-20 18:34:55 +0100

    Task-01: Retrieve all accounts - GREEN
    Returns a hardcoded a list of account.

--- src/main/java/com/assessment/account/service/AccountServiceImpl.java
+++ src/main/java/com/assessment/account/service/AccountServiceImpl.java
@@ -3,6 +3,7 @@ package com.assessment.account.service;
 import com.assessment.account.model.Account;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -10,6 +11,6 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public List<Account> retrieveAllAccounts() {
-        throw new RuntimeException("Not implemented yet.");
+        return Arrays.asList(new Account());
     }
 }
