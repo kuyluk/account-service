993d276e4bb248371bb70a790846dd23cae3b5c6
Murat Kuyluk <murat.kuyluk@gmail.com>
2019-05-20 13:09:17 +0100

    Task-01: Retrieve all accounts - GREEN
    Adds retrieve accounts endpoint that is basic and returns 200 OK.

--- src/main/java/com/assessment/account/controller/AccountController.java
+++ src/main/java/com/assessment/account/controller/AccountController.java
@@ -1,5 +1,15 @@
 package com.assessment.account.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AccountController {
+
+
+    @GetMapping(value = "accounts")
+    public void retrieveAllAccounts(){
+
+    }
 
 }
